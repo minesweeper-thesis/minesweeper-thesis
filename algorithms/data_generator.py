ae0d771a6c2b8159f98ac3468fd65ae3b4555951
Krzysztof Kapera <krzkapera@gmail.com>
2025-04-10 12:36:14 +0200

    append to file in data_generator

--- algorithms/data_generator.py
+++ algorithms/data_generator.py
@@ -1,45 +1,42 @@
 import json
-import os
 import random
 from algorithms.random_board import RandomBoard
 from algorithms.all_fields import all_fields
 from algorithms.is_solvable import is_solvable
 
+
 class DataGenerator:
-    def __init__(self, rows : int, columns : int, mine_count : int) -> None:
+    def __init__(self, rows: int, columns: int, mine_count: int) -> None:
         self.rows = rows
         self.columns = columns
         self.mine_count = mine_count
 
-        self.filename = 'data/'+str(rows)+','+str(columns)+',' + str(mine_count) + '.json'
-    
-    def generate(self, count : int) -> None:
-        if not os.path.exists(self.filename):
-            data = []
-        else:
-            with open(self.filename, "r") as f:
-                data = []
-                for line in f:
-                    data.append(json.loads(line))
-        
-        fields = all_fields(self.rows, self.columns, (-2,-2))
+        self.filename = (
+            "data/" + str(rows) + "," + str(columns) + "," + str(mine_count) + ".json"
+        )
+
+    def generate(self, count: int) -> None:
+        data = []
+
+        fields = all_fields(self.rows, self.columns, (-2, -2))
 
         for i in range(count):
             print(i)
 
-            start_field = fields[random.randint(0,len(fields)-1)]
+            start_field = fields[random.randint(0, len(fields) - 1)]
             board = RandomBoard(self.rows, self.columns, start_field, self.mine_count)
             solvable = is_solvable(board)
             board_json = board.to_json()
-            board_json['solvable'] = solvable
+            board_json["solvable"] = solvable
             data.append(board_json)
-        
-        with open(self.filename, "w") as f:
+
+        with open(self.filename, "a") as f:
             for instance in data:
                 json.dump(instance, f)
-                f.write('\n')
+                f.write("\n")
 
-for j in range(10):
-    print('\n',j,'\n')
-    a = DataGenerator(16, 16, 40)
+
+for j in range(50):
+    print("\n", j, "\n")
+    a = DataGenerator(16, 30, 99)
     a.generate(100)
