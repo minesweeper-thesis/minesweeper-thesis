28634be0ab514cabee8a44defa50dd24e79a26c3
Krzysztof Kapera <krzkapera@gmail.com>
2025-10-21 02:58:43 +0200

    board generation

--- backend/models/board.py
+++ backend/models/board.py
@@ -1,7 +1,7 @@
 import uuid
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, Index, UniqueConstraint
+from sqlalchemy import JSON, ForeignKey, Index, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
@@ -17,14 +17,16 @@ class BoardType(Base):
     rows: Mapped[int] = mapped_column(nullable=False)
     columns: Mapped[int] = mapped_column(nullable=False)
     mine_count: Mapped[int] = mapped_column(nullable=False)
-    start_field: Mapped[str] = mapped_column(nullable=False)
 
     __table_args__ = (
-        Index("ix_boardtype", "mine_count", "start_field"),
-        UniqueConstraint("mine_count", "start_field", name="uq_boardtype_tuple"),
+        Index("ix_boardtype", "rows", "columns", "mine_count"),
+        UniqueConstraint("rows", "columns", "mine_count", name="uq_boardtype_tuple"),
     )
 
     boards: Mapped[list["Board"]] = relationship("Board", back_populates="board_type")
+
+
+type BoardGrid = list[tuple[int, int]]
 
 
 class Board(Base):
@@ -34,7 +36,7 @@ class Board(Base):
     board_type_id: Mapped[uuid.UUID] = mapped_column(
         ForeignKey("board_type.id"), nullable=False, index=True
     )
-    minedfields: Mapped[str] = mapped_column(unique=True, nullable=False)
+    minedfields: Mapped[BoardGrid] = mapped_column(JSON, unique=True, nullable=False)
 
     board_type: Mapped[BoardType] = relationship("BoardType", back_populates="boards")
     gameplays: Mapped[list["Gameplay"]] = relationship(
