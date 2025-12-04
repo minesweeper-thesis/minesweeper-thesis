d5ce84996f52fed684ee638f6d1b517836b72e3f
Krzysztof Kapera <krzkapera@gmail.com>
2025-12-04 18:19:02 +0100

    migration fix

--- backend/db/versions/9f675e99b625_save_generator_settings_with_board.py
+++ backend/db/versions/9f675e99b625_save_generator_settings_with_board.py
@@ -8,9 +8,8 @@ Create Date: 2025-11-28 01:05:12.982653
 
 from typing import Sequence, Union
 
+import sqlalchemy as sa
 from alembic import op
-
-from backend.repositories.orm.board_orm import BoardORM
 
 # revision identifiers, used by Alembic.
 revision: str = "9f675e99b625"
@@ -20,8 +19,14 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.drop_table("boards")
-    BoardORM.__table__.create(op.get_bind())  # type: ignore
+    op.execute(sa.delete(sa.table("multiplayer_gameplays")))
+    op.execute(sa.delete(sa.table("multiplayer_rounds")))
+    op.execute(sa.delete(sa.table("singleplayer_gameplays")))
+    op.execute(sa.delete(sa.table("boards")))
+    op.add_column(
+        "boards",
+        sa.Column("generation_settings", sa.JSON(), nullable=False),
+    )
 
 
 def downgrade() -> None:
