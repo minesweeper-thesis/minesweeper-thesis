4a7f65f5369decdd045634fc68cf827035f4f7f3
Krzysztof Kapera <krzkapera@gmail.com>
2025-12-07 12:17:58 +0100

    round ready difficulty level

--- backend/services/multi/start_round_service.py
+++ backend/services/multi/start_round_service.py
@@ -106,7 +106,11 @@ class StartRoundService:
             for user_id in session.player_ids:
                 await self.game_transport.send(
                     user_id,
-                    RoundReady(session.id, next_round_index),
+                    RoundReady(
+                        session.id,
+                        next_round_index,
+                        session.game_config.difficulty_level,
+                    ),
                 )
 
             if session.is_next_round_available:
